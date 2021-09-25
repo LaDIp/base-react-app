9415f65435ed62db39e43b15297901870040ff38
LaDIp <laa2014@gmail.com>
2021-09-25 11:12:25 +0300

    Test

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,10 @@ import ReactDOM from '
 react- dom'
 
 ReactDOM.render(<h1>Привет, мир</h1>, document.getElementById('root'))
+
+  // "husky": {
+  //   "hooks": {
+  //     "pre-commit": "npm test && npm run lint",
+  //     "pre-push": "npm test && npm run lint"
+  //   }
+  // },
